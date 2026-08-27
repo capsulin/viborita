c871b6cf4a851ba99ce91e404a1fa88203188dd5
capsulin <saii@isseg.gob.mx>
2026-08-27 12:34:34 -0600

    Add asset paths for snake and food images

--- js/config.js
+++ js/config.js
@@ -1,1 +1,51 @@
+const ASSETS = {
 
+    snake: {
+
+        head: {
+            right: "assets/snake/head/head-right.png",
+            left: "assets/snake/head/head-left.png",
+            up: "assets/snake/head/head-up.png",
+            down: "assets/snake/head/head-down.png"
+        },
+
+        headBlink: {
+            right: "assets/snake/head/head-right-blink.png",
+            left: "assets/snake/head/head-left-blink.png",
+            up: "assets/snake/head/head-up-blink.png",
+            down: "assets/snake/head/head-down-blink.png"
+        },
+
+        headEat: {
+            right: "assets/snake/head/head-right-eat.png",
+            left: "assets/snake/head/head-left-eat.png",
+            up: "assets/snake/head/head-up-eat.png",
+            down: "assets/snake/head/head-down-eat.png"
+        },
+
+        body: {
+            horizontal: "assets/snake/body/body-lr.png",
+            vertical: "assets/snake/body/body-ud.png"
+        },
+
+        corners: {
+            upRight: "assets/snake/corners/corner-up-right.png",
+            upLeft: "assets/snake/corners/corner-up-left.png",
+            downRight: "assets/snake/corners/corner-down-right.png",
+            downLeft: "assets/snake/corners/corner-down-left.png"
+        },
+
+        tail: {
+            right: "assets/snake/tail/tail-right.png",
+            left: "assets/snake/tail/tail-left.png",
+            up: "assets/snake/tail/tail-up.png",
+            down: "assets/snake/tail/tail-down.png"
+        }
+
+    },
+
+    food: {
+        apple: "assets/food/apple.png"
+    }
+
+};
