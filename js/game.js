4d80b95d964ba21411c65618d0920ce0e8acf153
capsulin <saii@isseg.gob.mx>
2026-08-27 12:36:07 -0600

    Add snake head image rendering to game

--- js/game.js
+++ js/game.js
@@ -1,1 +1,18 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
+const head = new Image();
+
+head.src = ASSETS.snake.head.right;
+
+head.onload = function () {
+
+    ctx.drawImage(
+        head,
+        0,
+        0,
+        40,
+        40
+    );
+
+};
